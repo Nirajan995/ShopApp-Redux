cb08ce7e6cf894c2b0f3b0e292bfa033625f64b0
Nirajan Kunwor <nirajankunwor604@gmail.com>
2022-11-26 12:23:10 +0545

    feat: added product listing components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-
+import "./App.css"
+import Header from "./containers/Header";
+import { BrowserRouter as Router, Routes , Route} from "react-router-dom"; 
+import ProductListing from "./containers/ProductListing";
+import ProductDetail from "./containers/ProductDetail";
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" exact element={<ProductListing/>}/>
+          <Route path="/product/:productId" exact element={<ProductDetail/>}/>
+          <Route>404 not found!</Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -8,11 +8,11 @@ const initialState = {
    }]
 }
 
-export const productReducer = (state = initialState, action) => {
-   switch (action.type) {
+export const productReducer = (state = initialState, {type, payload}) => {
+   switch (type) {
       case ActionTypes.SET_PRODUCTS:
          return state;
       default:
-         break;
+         return state;
    }
 }
